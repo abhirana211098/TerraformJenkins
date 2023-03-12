4354663ee0c9554d0f02f08a55965cd362f8776e
abhirana211098 <121804764+abhirana211098@users.noreply.github.com>
2023-03-12 13:52:05 +0530

    updated credentials

--- Instance.tf
+++ Instance.tf
@@ -1,18 +1,18 @@
 
 provider "aws" {
   region  = "ap-south-1"
-  access_key="access-keeyyyyyyyyy"
-  secret_key="secret-keeyyyuyyy"
+  access_key="AKIATX5K52TFTD3OZY7S"
+  secret_key="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"
   
 }
 
 resource "aws_instance" "terraform_EC2" {
-  ami           = "ami-09ba48996007c8b50"
+  ami           = "ami-0d81306eddc614a45"
   instance_type = "t2.micro"
   security_groups=["launch-wizard-3"]
   associate_public_ip_address = true
   key_name = "secondawsfile"
-  count = 6
+  count = 
 
   tags = {
     Name ="Cluster"
